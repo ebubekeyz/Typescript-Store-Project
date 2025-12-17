cedfe93dadb48f37bf886efda62e674c6844220f
ebubekeyz <ebubeofforjoe@gmail.com>
2025-12-17 02:47:03 -0800

    2

--- src/utils/customFetch.ts
+++ src/utils/customFetch.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 let productionUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:4000/api"
-    : "https://node-store-project.onrender.com";
+    : "https://node-store-project.onrender.com/api";
 
 export const customFetch = axios.create({
   baseURL: productionUrl,

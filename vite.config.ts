c71cfce6f1beaddcf57e3aabe9978893026dd628
ebubekeyz <ebubeofforjoe@gmail.com>
2025-12-18 01:39:23 -0800

    4

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // ADD THIS
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
